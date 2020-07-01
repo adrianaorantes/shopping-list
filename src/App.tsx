20639a3e9357dc07b2217044d514ff42ee8e8950
adrianaorantes <aorantes801@gmail.com>
2020-07-01 18:56:59 -0400

    fifth commit - added form and input

--- src/App.tsx
+++ src/App.tsx
@@ -5,69 +5,89 @@ import { listenerCount } from 'process';
 
 
 //we need a class shopping item
-//we create classes to be good at organizin gour code 
-//create an pbject wehen we need to add somethign to the list 
-//newItem = new ShoppingItem();
 class ShoppingItem{
   public name = "";
   public price = 0.0;
 }
 
-//creating instances of ShoppingItem class
+/*creating instances of ShoppingItem class
 const pizza: ShoppingItem = {name: "Pizza", price: 4.5 }
 const eggs: ShoppingItem = {name: "Eggs", price: 1.5 }
 const milk: ShoppingItem = {name: "Milk", price: 2.0 }
+*/
 
 //define the state that contains current todo list
-//define what an object should look like to any code that access it 
-//define properties 
 interface AppState{
   items: Array<ShoppingItem>;
-  //items.add(pizza);
 }
 
 
 //we switch from function to class bc functions do a specific thing and classes do multiple things
 class App extends React.Component<{}, AppState>{
-  //runs whenevr a new class object is created
-  //a class is not just for defining what an object looks like, also how it works
-  //interface defines what an object looks like
-  //class is more complicated than an interface
-  //when you create an object instance of a class the constructor runs
-  //anything you need to do when you run an object
+  
   constructor(props: any, state: AppState) {
     super(props, state);
-    this.state = { items: [pizza, milk, eggs] };
-    
-    //this.state.items.push(milk);
+    this.state = { items: [] };
+
   }
   //renders the html (interprets) to display content to a page
-  //gets called every single time react detetcts a change in one of my compoenents
   render(){
   return (
     <div className="App">
       <header className="App-header">
           <b>Shopping List</b>
-          <p> This is a shopping list app! You can add a item to the list below, along with the price. Enjoy! </p>
+          <p> This is a shopping list app! You can add an item to the list below, along with the price. Enjoy! </p>
       </header>
       <h1> These are the items: </h1>
 
-
+      <ul>
           {/* //turns array of items into a map */}
-          {this.state.items.map((item) => (
-          <li>
-            Name of product: {item.name}
-          <br />
-          Price: ${item.price}
-          </li>
+          {this.state.items.map(item => (
+            <li>
+              Name of Product: {item.name}
+            <br />
+              Price: ${item.price}
+            </li>
           ))}
-
-
-      <p>  </p>
+        </ul>
+        <p>  </p>
+        <form onSubmit={this.submitForm}>
+          <input type="text" placeholder="Enter a name..." onChange={this.changeInputName} />
+          <input type="text" placeholder="Price..." onChange={this.changeInputPrice} />
+          <input type="submit" placeholder="Submit"/>
+        </form>
+        <br></br>
+        <br></br>
     </div>
   );
   }
-}
+  private inputName = "";
+  private inputPrice = "";
 
+  private changeInputName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.inputName = event.target.value;
+  }
+  private changeInputPrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.inputPrice = event.target.value;
+  }
+
+  // constraining the type of the event 
+  private submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    /*generate an alert with the value of the textbox
+    alert("Name: " + this.inputName + "\nPrice: " + this.inputPrice);*/
+  
+    // updating the state of the component
+    //create a new ShoppingItem object with private variables
+    const newItem: ShoppingItem = {name: this.inputName, price: parseFloat(this.inputPrice)}
+
+
+    this.setState({
+      //create new array list of type ShoppingItem and concatanate original items list with new item object
+      items: this.state.items.concat(newItem)
+    });
+
+  };
+}
 
 export default App;
