aa8fa90ccdcc914759c3bdf77ae8920c222fc418
adrianaorantes <aorantes801@gmail.com>
2020-07-01 12:34:42 -0400

    first commit - created class, interface, and added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactComponent } from '*.svg';
 
-function App() {
+
+//we need a class shopping item
+//we create classes to be good at organizin gour code 
+//create an pbject wehen we need to add somethign to the list 
+//newItem = new ShoppingItem();
+class ShoppingItem{
+  public name = "";
+  public price = 0.0;
+}
+
+//define the state, it is a list
+interface AppState{
+  items: Array<string>;
+}
+
+//we switch from function to class bc functions do a specific thing and classes do multiple things
+class App extends React.Component<{}, AppState>{
+  //renders the html (interprets)
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Shopping list
       </header>
+      <h1> These are the items: </h1>
     </div>
   );
+  }
 }
 
 export default App;
